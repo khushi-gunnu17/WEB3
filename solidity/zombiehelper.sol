1c93bd46fb67325b7e63b1e480318d6302403462
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-07-15 16:16:37 +0530

    adding functionalities to zombiehelper

--- solidity/zombiehelper.sol
+++ solidity/zombiehelper.sol
@@ -6,9 +6,28 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+    uint levelUpFee = 0.001 ether;
+
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
+    }
+
+
+    function withdraw() external onlyOwner {
+        address payable _owner = payable(owner());
+        _owner.transfer(address(this).balance);
+    }
+
+
+    function setLevelUpFee(uint _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }
+
+
+    function levelUp(uint _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
     }
 
 
@@ -27,16 +46,14 @@ contract ZombieHelper is ZombieFeeding {
     function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
 
         uint[] memory result = new uint[](ownerZombieCount[_owner]);
-
         uint counter = 0;
 
-        for (uint i = 0 ; i < zombies.length ; i++) {
+        for (uint i = 0; i < zombies.length; i++) {
             if (zombieToOwner[i] == _owner) {
                 result[counter] = i;
                 counter++;
             }
         }
-
         return result;
     }
 
